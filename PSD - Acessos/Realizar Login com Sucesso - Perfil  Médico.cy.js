78aa394c886ca3791a0c00b46597da5128ac33f8
Rian Barbosa dos Santos <rian.santos_ext@interplayers.com.br>
2023-01-05 08:18:43 -0300

    Update Realizar Login com Sucesso - Perfil  Médico.cy.js

--- PSD - Acessos/Realizar Login com Sucesso - Perfil  Médico.cy.js
+++ PSD - Acessos/Realizar Login com Sucesso - Perfil  Médico.cy.js
@@ -5,6 +5,6 @@ describe ('Realizar Login no Ambiente DEV', function() {
         cy.visit('https://idpapp-psd.azurewebsites.net/amgen/rastrear/app/login')
         cy.get('#txtUser').type('rian.medico@mailinator.com')
         cy.get('#password').type('Ecs@123456')
-        cy.contains('Entrar').click().wait(15000)
+        cy.contains('Entrar').click()
     })
 })
